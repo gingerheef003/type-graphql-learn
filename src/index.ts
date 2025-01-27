740117ebe5d18029b14bc3a8ac579599a1abdca1
Ginger Heef <gingerheef003@gmail.com>
2025-01-27 21:02:24 +0530

    register resolver setup

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,14 @@ import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
 import { buildSchema, Query, Resolver } from "type-graphql";
 import express from "express";
-import cors from 'cors';
+import cors from "cors";
+import { AppDataSource } from "../data-source";
+import { RegisterResolver } from "./modules/user/Register";
 
-@Resolver()
-class HelloResolver {
-  @Query(() => String)
-  async hello() {
-    return "Hello World!";
-  }
-}
 
 const bootstrap = async () => {
   const schema = await buildSchema({
-    resolvers: [HelloResolver],
+    resolvers: [RegisterResolver],
   });
 
   const apolloServer = new ApolloServer({
@@ -24,15 +19,24 @@ const bootstrap = async () => {
   await apolloServer.start();
 
   const app = express();
-  app.use('/graphql',
+  app.use(
+    "/graphql",
     cors<cors.CorsRequest>(),
     express.json(),
-    expressMiddleware(apolloServer),
-  )
+    expressMiddleware(apolloServer)
+  );
 
   app.listen(4000, () => {
     console.log("server started on http://localhost:4000/graphql");
   });
 };
 
-bootstrap();
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Data Source has been initialized");
+    bootstrap();
+  })
+  .catch((err) => {
+    console.error("Error during Data Source initialization", err);
+  });
+
