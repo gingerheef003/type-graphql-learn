07c84b45bc77fb2d498f9da01981838a28dbbe5c
Ginger Heef <gingerheef003@gmail.com>
2025-02-24 12:53:52 +0530

    added query complexity and fixed password input

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ import { RedisStore } from "connect-redis";
 import dotenv from "dotenv";
 import { redis } from "./redis";
 import { createSchema } from "./utils/createSchema";
+import { fieldExtensionsEstimator, getComplexity, simpleEstimator } from "graphql-query-complexity";
 
 dotenv.config();
 if (!process.env.SESSION_SECRET) {
@@ -16,12 +17,37 @@ if (!process.env.SESSION_SECRET) {
 }
 
 const SESSION_SECRET = process.env.SESSION_SECRET;
+const MAX_COMPLEXITY = 8;
 
 const bootstrap = async () => {
   const schema = await createSchema();
 
   const apolloServer = new ApolloServer({
     schema,
+    plugins: [
+      {
+        requestDidStart: async() => ({
+          async didResolveOperation({ request, document }) {
+            const complexity = getComplexity({
+              schema,
+              operationName: request.operationName,
+              query: document,
+              variables: request.variables,
+              estimators: [
+                fieldExtensionsEstimator(),
+                simpleEstimator({defaultComplexity: 1})
+              ]
+            })
+
+            if (complexity > MAX_COMPLEXITY) {
+              throw new Error(`Sorry, too complicated query! ${complexity} exceeded the maximum allowed complexity of ${MAX_COMPLEXITY}`)
+            }
+
+            console.log("Used query complexity points:", complexity);
+          }
+        })
+      }
+    ],
   });
   await apolloServer.start();
 
--- src/modules/shared/PasswordInput.ts
+++ src/modules/shared/PasswordInput.ts
@@ -1,11 +1,11 @@
-import { Min } from "class-validator";
+import { MinLength } from "class-validator";
 import { ClassType, Field, InputType } from "type-graphql";
 
 export const PasswordMixin = <T extends ClassType>(BaseClass: T) => {
   @InputType()
   class PasswordInput extends BaseClass {
     @Field()
-    @Min(5)
+    @MinLength(5)
     password!: string;
   }
 
